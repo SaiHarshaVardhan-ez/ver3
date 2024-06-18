f3ac64992831123efc2b7fe8883bfffe3307fdce
leocodeio <basicleo@787>
2024-06-18 09:38:06 +0530

    applied basic css

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Editor = () => {
   return (
-    <>
-      editor area
-    </>
-  )
-}
+    <div className="flex flex-col w-full h-full items-center justify-center">
+      <h2>
+        <b>Editor Area</b>
+      </h2>
+      <div
+        className="w-full h-full text-center font-bold bg-white p-2 rounded shadow-lg overflow-auto border border-black rounded"
+        contentEditable="true"
+      ></div>
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;
